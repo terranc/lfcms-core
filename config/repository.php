c21ce563475e575593bd5e783bede3d105729a59
terranc <wuchaoboy@gmail.com>
2017-07-07 01:08:37 +0800

    delete repository.php

--- config/repository.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'directory_path' => 'App' . DIRECTORY_SEPARATOR . 'Repositories',
-    'repository_namespace' => 'App\Repositories',
-    'model_namespace' => 'App\Models'
-];
